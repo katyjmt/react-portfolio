e0385930ae5b95c01e7af0613c710928d3a288c3
Kate Thompson <katyjmt@gmail.com>
2023-10-16 10:28:48 +1100

    Updated About bio

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,13 +15,13 @@ export default function Home() {
       <div className="intro-container">
         <p className="intro">
           Hi, I&apos;m Katy! I&apos;ve been working in the tech industry for 7
-          years, over which time I&apos;ve worn multiple hats as project
+          years. Over this time, I&apos;ve worn multiple hats as project
           manager, front-end developer, UX consultant, designer, strategist,
           data analyst and account manager.
           <br />
           <br />
           I love creating beautiful and useful things, and get deep satisfaction
-          out of working out how to do something from first principles. Over the
+          out of understanding things from first principles. Over the
           years I&apos;ve done self-driven deep-dives into front-end coding,
           SQL, typeface design, book binding, sewing and pattern making, how to
           fit-out a camper van, and lots of other creative and analytical
@@ -33,8 +33,8 @@ export default function Home() {
           projects for renowned brands like Sydney Symphony Orchestra, Bega,
           Mars, Fitness First, Warner Music, and more. The projects I&apos;ve
           worked on, and the people I&apos;ve worked with have taught me a lot
-          about what good design is. How to create something that&apos;s not
-          just aesthetically delightful, but that is designed around a deep
+          about what good design is. How to create things that are not
+          just aesthetically delightful, but that are designed around a deep
           understanding of the user and their needs.
           <br />
           <br />

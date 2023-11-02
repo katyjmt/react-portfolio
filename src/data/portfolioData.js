acc88dab06b126559a0c0e377cb3be42bdfc3528
Kate Thompson <katyjmt@gmail.com>
2023-11-02 18:15:02 +1100

    Added new project to portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -2,7 +2,7 @@ import horiseonImg from "../assets/horiseonImg.png"
 import evieImg from "../assets/evieImg.png"
 import bakeNGrooveImg from "../assets/bakeNGrooveImg.png"
 import fashionImg from "../assets/404Img.png"
-import extraImg1 from "../assets/extraImg1.jpeg"
+import psImg from "../assets/ps-screenshot.png"
 import extraImg2 from "../assets/extraImg2.jpg"
 
 const projectData = [
@@ -30,7 +30,7 @@ const projectData = [
     link: "https://fashion404-1b1ca75a43ac.herokuapp.com/",
     github: "https://github.com/ecommerce-fashion-designer/fashion-designer",
   },
-  { name: "Coming Soon 1", imageRef: extraImg1, link: "#", github: "" },
+  { name: "Planner Page Builder", imageRef: psImg, link: "https://pure-reef-32089-709794f662d7.herokuapp.com/", github: "https://github.com/katyjmt/ps-lite" },
   { name: "Coming Soon 2", imageRef: extraImg2, link: "#", github: "" },
 ];
 
